b8046c8e12f634458e8579cc5cbd2310a3a2dafc
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2015-07-07 21:58:20 +0300

    prettify output

--- github-telegram-hook.rkt
+++ github-telegram-hook.rkt
@@ -17,22 +17,21 @@
   (~a (hash-ref data 'name)))
 
 (define (get-repo data)
-  (~a (hash-ref (hash-ref data 'owner) 'name) "/" (hash-ref data 'name) " <" (hash-ref data 'url) ">")
-  )  
+  (~a (hash-ref data 'full_name) " (" (hash-ref data 'url) ")"))
 
 (define (get-commit data)
-  (~a " - "(hash-ref data 'message) " (" (get-author (hash-ref data 'author)) ") " (hash-ref data 'url)))
+  (~a "➕ "(hash-ref data 'message) " (" (get-author (hash-ref data 'author)) ") " (hash-ref data 'url)))
 
 (define (create-push-notification data)
-  `(,(~a "Commits pushed by " (get-author (hash-ref data 'pusher)) " in " (get-repo (hash-ref data 'repository)) ":")
+  `(,(~a "📥 " (get-author (hash-ref data 'pusher)) " pushed to " (get-repo (hash-ref data 'repository)) ":")
     ,@(map get-commit (hash-ref data 'commits))
-    ,(~a "Compare: " (hash-ref data 'compare))))
+    ,(~a "🔎 Diff: " (hash-ref data 'compare))))
 
 (define (hook req)
   (let ((body (bytes->jsexpr (request-post-data/raw req))))
-    (sendMessage (string-join (create-push-notification body)) "\n")
+    (sendMessage (string-join (create-push-notification body) "\n"))
     (response 200 #"OK" (current-seconds) #f empty void)))
 
 (serve/servlet hook
                #:port 8080
-               #:servlet-path "/guthub")
+               #:servlet-path "/github")
