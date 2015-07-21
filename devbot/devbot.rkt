3b9f0ef98cc52b583e2f23296dcfdaf53fd4b9d2
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2015-07-21 14:12:04 +0300

    some code to remove keyboard from chat

--- devbot/devbot.rkt
+++ devbot/devbot.rkt
@@ -65,12 +65,17 @@
     (close-input-port (get-pure-port uri))))
 
 (define (buildbot/select-builder-keyboard)
-  (let ((builders (buildbot/get-builders)))
+    (let ((builders (buildbot/get-builders)))
     (jsexpr->string (hash
-                     'keyboard (map (lambda (b) (list b)) builders)
-                     'selective #t
-                     'resize_keyboard #t
-                     'one_time_keyboard #t))))
+                     'hide-keyboard #t
+                     'selective (not for-all-users)))))
+;;  (let ((builders (buildbot/get-builders)))
+;;    (jsexpr->string (hash
+;;                     'keyboard (map (lambda (b) (list b)) builders)
+;;                     'selective #t
+;;                     'resize_keyboard #t
+;;                     'one_time_keyboard #t))))
+
 
 (define (telegram/send-select-builder-keyboard message-id)
   (let* ((markup (buildbot/select-builder-keyboard))
