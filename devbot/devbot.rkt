4020ffae6babc20c89b3daf0d2e35e1942721bd7
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2015-07-21 17:55:35 +0300

    uncomment server starting lines

--- devbot/devbot.rkt
+++ devbot/devbot.rkt
@@ -175,16 +175,12 @@
    [("telegram") #:method "post" telegram-hook]
    [("builds") #:method "post" builds-hook]))
 
-(define (start request)
-  (displayln request)
-  (hook-dispatch request))
+(telegram/set-webhook telegram/webhook)
+(telegram/send-message "Всем пони!")
 
-;;(telegram/set-webhook telegram/webhook)
-;;(telegram/send-message "Всем пони!")
-
-;;(serve/servlet hook-dispatch
-;;               #:port 8080 
-;;               #:servlet-path ""
-;;               #:servlet-regexp #rx""
-;;               #:listen-ip #f
-;;               #:command-line? #t)
+(serve/servlet hook-dispatch
+               #:port 8080 
+               #:servlet-path ""
+               #:servlet-regexp #rx""
+               #:listen-ip #f
+               #:command-line? #t)
