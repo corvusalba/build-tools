3329e50133f6d8c627ead88db6273e6f2e4beddd
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2015-08-28 17:35:41 +0300

    fix

--- devbot/devbot.rkt
+++ devbot/devbot.rkt
@@ -105,8 +105,8 @@
 (define (telegram/send-message text)
   (let ((uri (compose-url telegram/send-message-url
                           (query (key-value "chat_id" telegram/chat_id)
-                                 (key-value "disable_web_page_preview" #t)
-                                 (key-value "text" text)))))
+                                 (key-value "text" (uri-encode text))
+                                 (key-value "disable_web_page_preview" #t)))))
     (close-input-port (get-pure-port uri))))
 
 (define (telegram/send-select-builder-keyboard message-id)
